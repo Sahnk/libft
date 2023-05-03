d05b265800e62bdb239c787974564b7311d71914
Juliany Bernardo <julianybernardo@outlook.com>
2023-05-03 16:35:41 +0000

    Functions addition

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julberna <julberna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 01:01:16 by julberna          #+#    #+#             */
+/*   Updated: 2023/05/03 01:04:48 by julberna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size <= dest_len)
+		return (size + src_len);
+	if (dest_len > 0)
+	{
+		while ((src[i] != '\0') && (i < size) && (dest[dest_len + i] != '\0'))
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
+		dest[dest_len + i] = '\0';
+	}
+	return (dest_len + src_len);
+}
